d537c0dbf1fcfab565b2ba53a9613b4383a383f9
DocSJ1980 <docsaj@outlook.com>
2023-04-29 00:52:06 +0500

    Socket-io added realtime messages showing

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ import cors from "cors"
 import chatRouter from "./routes/chatRoutes.js"
 import messageRouter from "./routes/messageRoutes.js"
 import userRouter from "./routes/userRoutes.js"
+import { createServer } from "http"
+import { Server } from "socket.io";
 
 //. Declaring Path for dotenv
 config({
@@ -31,6 +33,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
 
+
 //. Using routes
 app.get("/", (req, res) => {
     res.send("<h1>App Working fine</h1>")
@@ -42,8 +45,50 @@ app.use("/api/user", userRouter)
 //. Connecting Database
 connectDB(URI)
 
+const http = createServer(app)
+const io = new Server(http, {
+    cors: {
+        origin: "http://localhost:5173",
+    },
+})
+
+let activeUsers = [];
+
+io.on("connection", (socket) => {
+    // add new User
+    socket.on("new-user-add", (newUserId) => {
+        // if user is not added previously
+        if (!activeUsers.some((user) => user.userId === newUserId)) {
+            activeUsers.push({ userId: newUserId, socketId: socket.id });
+            console.log("New User Connected", activeUsers);
+        }
+        // send all active users to new user
+        io.emit("get-users", activeUsers);
+    });
+
+    socket.on("disconnect", () => {
+        // remove user from active users
+        activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
+        console.log("User Disconnected", activeUsers);
+        // send all active users to all users
+        io.emit("get-users", activeUsers);
+    });
+
+    // send message to a specific user
+    socket.on("send-message", (data) => {
+        const { receiverId } = data;
+        const user = activeUsers.find((user) => user.userId === receiverId);
+        console.log("Sending from socket to :", receiverId)
+        console.log("Data: ", data)
+        if (user) {
+            io.to(user.socketId).emit("recieve-message", data);
+        }
+    });
+});
+
+
 //. Running http server
-app.listen(port, () => {
+http.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
 })
 
